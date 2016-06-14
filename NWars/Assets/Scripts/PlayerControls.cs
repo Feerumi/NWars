f9b728d9292d2a10635a097a1ed1af9b32abdaea
Henri Laiho <henri.k.laiho@gmail.com>
2016-06-14 12:50:54 +0300

    Added dash to player movement
    Updated movementScene
    Updated playerControls
    
    Press A on controller to dash (fire1)

--- NWars/Assets/Scripts/PlayerControls.cs
+++ NWars/Assets/Scripts/PlayerControls.cs
@@ -6,16 +6,31 @@ public class PlayerControls : MonoBehaviour {
 
 	public float movementSpeed;
 	public float turningSpeed;
+	public float dashMultiplier;
+	public float dashDuration;
 	private Vector3 direction = Vector3.zero;
 	private CharacterController controller;
 	private bool isMoving;
 
+	private bool dashing;
+	private float dashSpeed;
+	private float dashTimerInSeconds;
+
 	void Start () {
 		controller = GetComponent<CharacterController> ();
 		isMoving = false;
+		dashing = false;
+		dashSpeed = movementSpeed * dashMultiplier;
+		dashTimerInSeconds = 0f;
 	}
 
 	void Update () {
+
+		if (Input.GetButtonDown("Fire1") && !dashing) {
+			dashing = true;
+			dashTimerInSeconds = dashDuration;
+		}
+
 		float horizontal = Input.GetAxisRaw ("Horizontal");
 		float vertical = Input.GetAxisRaw ("Vertical");
 
@@ -26,7 +41,12 @@ public class PlayerControls : MonoBehaviour {
 		if (isMoving) {
 
 			direction = new Vector3 (Input.GetAxisRaw ("Horizontal"), 0, Input.GetAxisRaw ("Vertical"));
-			direction *= movementSpeed;
+
+			if (dashing) {
+				direction *= dashSpeed;
+			} else {
+				direction *= movementSpeed;
+			}
 
 			if (direction != Vector3.zero) {
 				transform.rotation = Quaternion.LookRotation (direction);
@@ -34,7 +54,14 @@ public class PlayerControls : MonoBehaviour {
 
 			controller.Move (direction * Time.deltaTime);
 
-
 		}
+
+		if (dashTimerInSeconds > 0) {
+			dashTimerInSeconds -= Time.deltaTime;
+		} else {
+			dashTimerInSeconds = 0;
+			dashing = false;
+		}
+
 	}
 }
