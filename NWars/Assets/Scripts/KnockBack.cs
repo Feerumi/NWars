83e480e2f2450620f2e73fd48e50d0ac90bfc212
hklaiho <henri.k.laiho@gmail.com>
2016-06-15 12:21:54 +0300

    Added knockback to player
    Player know gets knockedback from throwableobjects hitting them.
    Throwable object needs to be set active at throw start to apply knockback on collision enter.

---
+++ NWars/Assets/Scripts/KnockBack.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KnockBack : MonoBehaviour {
+
+	Rigidbody rigidBody;
+	public float explosionForce;
+	public float explosionRadius;
+
+	// Use this for initialization
+	void Start () {
+		rigidBody = GetComponent<Rigidbody> ();
+	}
+
+	void OnCollisionEnter(Collision other){
+
+		if (other.gameObject.tag.Equals ("ThrowableObject")) {
+			ThrowableKnockBack values = other.gameObject.GetComponent<ThrowableKnockBack> ();
+			if (values.getActive()) {
+				rigidBody.AddExplosionForce (explosionForce * values.getMass(), other.transform.position, explosionRadius);	
+			}
+		}
+	}
+}
---
+++ NWars/Assets/Scripts/ThrowableKnockBack.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ThrowableKnockBack : MonoBehaviour {
+
+	public bool isActive;
+	Rigidbody rigidBody;
+	// Use this for initialization
+	void Start () {
+		rigidBody = GetComponent<Rigidbody> ();
+	}
+
+	public bool getActive(){
+		return isActive;
+	}
+
+	public float getMass(){
+		return rigidBody.mass;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
